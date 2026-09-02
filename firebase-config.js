1d10d851dad9101e8b5a86d85f0b82d6ed287c18
Tomideemus <87074253+Tomideemus@users.noreply.github.com>
2026-09-02 19:29:46 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,1 +1,9 @@
-
+const firebaseConfig = {
+  apiKey: "AIzaSyBB9isCK0gH8ZiFAYNAudlEzSUSfzx4MHI",
+  authDomain: "omkry-tasting-app.firebaseapp.com",
+  databaseURL: "https://omkry-tasting-app-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "omkry-tasting-app",
+  storageBucket: "omkry-tasting-app.firebasestorage.app",
+  messagingSenderId: "948515311102",
+  appId: "1:948515311102:web:86bd14ceb0a139a95684af"
+};
