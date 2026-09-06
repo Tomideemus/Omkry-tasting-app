ddf7e4ef1076e916038325d1f08c35409ec98ca5
Tomideemus <87074253+Tomideemus@users.noreply.github.com>
2026-09-06 22:07:14 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,23 +1,67 @@
-import { initializeApp } from
-  "https://www.gstatic.com/firebasejs/12.18.0/firebase-app.js";
+import {
+  initializeApp
+} from "https://www.gstatic.com/firebasejs/12.18.0/firebase-app.js";
 
 import {
   getDatabase
-} from
-  "https://www.gstatic.com/firebasejs/12.18.0/firebase-database.js";
+} from "https://www.gstatic.com/firebasejs/12.18.0/firebase-database.js";
 
+import {
+  getAuth
+} from "https://www.gstatic.com/firebasejs/12.18.0/firebase-auth.js";
+
+import {
+  getStorage
+} from "https://www.gstatic.com/firebasejs/12.18.0/firebase-storage.js";
+
+
+// Kopioi nämä arvot
+// Firebase Console >
+// Project settings >
+// Your apps
+//
+// ÄLÄ käytä alla olevia LISÄÄ_TÄHÄN-arvoja.
+// Korvaa ne omilla Firebase-arvoillasi.
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBB9isCK0gH8ZiFAYNAudlEzSUSfzx4MHI",
-  authDomain: "omkry-tasting-app.firebaseapp.com",
-  databaseURL: "https://omkry-tasting-app-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "omkry-tasting-app",
-  storageBucket: "omkry-tasting-app.firebasestorage.app",
-  messagingSenderId: "948515311102",
-  appId: "1:948515311102:web:86bd14ceb0a139a95684af"
+
+  apiKey:
+   "AIzaSyBB9isCK0gH8ZiFAYNAudlEzSUSfzx4MHI", 
+
+  authDomain:
+   "omkry-tasting-app.firebaseapp.com", 
+
+  databaseURL:
+   "https://omkry-tasting-app-default-rtdb.europe-west1.firebasedatabase.app",
+
+  projectId:
+    "omkry-tasting-app",
+  
+  storageBucket: 
+    "omkry-tasting-app.firebasestorage.app",
+
+  messagingSenderId:
+    "948515311102",
+
+  appId:
+    "1:948515311102:web:86bd14ceb0a139a95684af"
+
 };
 
 
-const app = initializeApp(firebaseConfig);
+const app =
+  initializeApp(
+    firebaseConfig
+  );
 
-export const database = getDatabase(app);
+
+export const database =
+  getDatabase(app);
+
+
+export const auth =
+  getAuth(app);
+
+
+export const storage =
+  getStorage(app);
